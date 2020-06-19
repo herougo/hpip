373395da30a7e6cee7d9437ed84511911701a9ac
herougo <henri.romel@adeptmind.ai>
2020-06-19 13:48:37 -0400

    hello world

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hpip
 A pip package for adding quick code into repositories when debugging, logging, etc.
+
+To make a pip package, have a setup.py file.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='H Pip',
+      version='1.0',
+      description='A pip package for adding quick code into repositories when debugging, logging, etc.',
+      author='',
+      author_email='',
+      url='',
+      packages=['hpip'],
+     )
